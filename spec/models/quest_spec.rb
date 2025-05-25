fdd5fdc53f9f2a33584f8d6bb18484bcf04c07d2
Nattapat Pinrat <nattapatpinrat@Nattapats-MacBook-Air-2.local>
2025-05-25 16:05:35 +0700

    change test

--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -3,10 +3,16 @@ require 'rails_helper'
 RSpec.describe Quest, type: :model do
   describe 'attributes' do
     it 'has name and status attrivutes' do
-    quest = Quest.new(name:'Test Quest',status: false)
-    
+    quest = Quest.new(name :'Test Quest', status: false)
     expect(quest.name).to eq('Test Quest')
     expect(quest.status).to eq(false)
+    end
+  end
+
+  describe 'validation' do
+    it 'is valid with only a name' do
+      quest = Quest.new(name :'Test Quest')
+      expect(quest).to be_valid
     end
   end
 end
