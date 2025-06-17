b3e98c0bf2ea6829c615e2bd4d823c2bc420f96e
Nattapat Pinrat <nattapatpinrat@Nattapats-MacBook-Air-2.local>
2025-06-18 00:32:03 +0700

    Add space in models

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,3 +1,11 @@
 class Quest < ApplicationRecord
-  validates :name, presence:true
+  validates :name, presence: true
+  
+  before_save :trim_whitespace
+  
+  private
+  
+  def trim_whitespace
+    self.name = name.strip if name.present?
+  end
 end
